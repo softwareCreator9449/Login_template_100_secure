65774448f65c2ed433bf8d6f37e0693899e27814
Nobody <nobody@example.com>
2024-02-28 13:25:28 +0100

    v1 - insert example

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require '../includes/db_connect.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = md5($_POST['password']); 
-
+    // Ejemplo -> INSERT INTO users (username, password) VALUES ('pedro', '1f8a558aa07de0d25c61e79f5d2ce1c3');
     $stmt = $conn->prepare("SELECT username FROM users WHERE username = ? AND password = ?");
     $stmt->bind_param("ss", $username, $password);
     $stmt->execute();
